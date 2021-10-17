5c437577ec291ae081fc90ac7a45023f821b6f36
Natho99 <92109769+Natho99@users.noreply.github.com>
2021-10-17 17:57:09 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Home</title>
+<link rel="stylesheet" type="text/css" href="index.css"/>
+</head>
+<body>
+	<div id="holder"><a href="https://wa.me/254700423736?
+		text=Congratulations%20Good%20work"> <img src="c.jpg " width="130px" height="100px"></a>
+		 <div id="header">
+		 
+    <ul>
+    	<li><a href="Acknowledgement.html">Acknowledgement</a></li>
+    	<li><a href="overview.html">Overview</a></li>
+    	<li><a href="MyStory.html">MyStory</a></li>
+        <li><a href="index.html">Home</a></li>
+    </ul>
+    </div><!--header -->
+	<div id="gallery">
+		<h1 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		color:#6340e0;">Gallery</h1>
+		<img src="6.jpg" width="470px" height="210px"/>
+		<img src="5.jpg" width="470px" height="200px"/>
+	
+	</div><!--gallery-->
+	<div id="contact">
+		<h1 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		color: #6340e0">Nathan Kimutai</h1>
+		<img src="9.jpg" width="350px" height="250px"/>
 
+		<div style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		color: black" id="b1">&nbsp;&nbsp;
+		<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0700423736</h2>
+		</div><!--b1-->
+			</div><!--contact-->
+</div><!--holder-->
+</body>
+</html>
